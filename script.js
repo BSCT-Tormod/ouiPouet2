f396c08de7997f7deed3a23ce67021c0976e2104
BSCT-Tormod <thomasdemeyere33@gmail.com>
2023-09-06 20:47:41 +0200

    V1 fonctionnelle

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ var cardsOwned = [];
 var sCardsOwned = [];
 var ssCardsOwned = [];
 
+var itemsCollection = {};
 var reveal = false;
 
 console.log("hello world le programme");
@@ -39,17 +40,31 @@ document.querySelector("body").onclick = function(){
         pickACard(1);
     }
 };
+document.querySelectorAll(".inventaire-item").forEach(element => {
+    element.addEventListener("mouseover", function () {
+        if (document.querySelector("#tab-inventaire-0").ariaSelected == "true"){
+            displayInfos(element.id.split("-")[1]);
+        } else if (document.querySelector("#tab-inventaire-1").ariaSelected == "true"){
+            displayInfos(parseInt(element.id.split("-")[1])+24);
+        }
+    });
+});
 document.querySelector("#achat-picks-up").onclick = function(){achatPicksUp()};
-document.querySelector("#tab-shop-0").onclick = function(){tabShop(0)};
-document.querySelector("#tab-shop-1").onclick = function(){tabShop(1)};
-document.querySelector("#tab-shop-2").onclick = function(){tabShop(2)};
-document.querySelector("#shop-close").onclick = function () {fermerShop();};
-document.querySelector("#ok-shop").onclick = function () {fermerShop();};
+document.querySelector("#tab-shop-0").onclick = function(){tabFenetre(0,"shop")};
+document.querySelector("#tab-shop-1").onclick = function(){tabFenetre(1,"shop")};
+document.querySelector("#tab-shop-2").onclick = function(){tabFenetre(2,"shop")};
+document.querySelector("#tab-inventaire-0").onclick = function(){tabFenetre(0,"inventaire"); afficherItems(1)};
+document.querySelector("#tab-inventaire-1").onclick = function(){tabFenetre(1,"inventaire"); afficherItems(2);};
+document.querySelector("#tab-inventaire-2").onclick = function(){tabFenetre(2,"inventaire")};
+document.querySelector("#shop-close").onclick = function () {fermerFenetre("shop");};
+document.querySelector("#ok-shop").onclick = function () {fermerFenetre("shop");};
+document.querySelector("#inventaire-close").onclick = function () {fermerFenetre("inventaire");};
+document.querySelector("#ok-inventaire").onclick = function () {fermerFenetre("inventaire");};
 document.querySelector(".btn-achat").onclick = function(){tirage()};
 document.querySelector(".picked-card-container").onclick = function(){pickACard()};
 // document.querySelector("#collection").onclick = function(){collection()};
-document.querySelector("#shop-button").onclick = function(){afficherShop()};
-document.querySelector(".collection-partir").onclick = function(){fermerCollection()};
+document.querySelector("#shop-button").onclick = function(){afficherFenetre("shop")};
+document.querySelector("#inventaire-button").onclick = function(){afficherFenetre("inventaire"); tabFenetre(0, "inventaire"); afficherItems(1)};
 
 
 ////// rompiche
@@ -70,8 +85,7 @@ async function increment(){
     await sleep(100);
     document.querySelector(".btn-achat").style.transition = "0.3s";
     document.querySelector(".btn-achat").style.fontSize = "30px";
-    autoStyleTirage();
-    autoStyleImage();
+    savePouet();
 }
 
 (function( d ) {
@@ -113,129 +127,8 @@ function autoHue(){
     return "rgb("+red+','+green+','+blue+")";
 }
 
-// gère automatiquement l'aspect de la bare de progression et du bouton d'achat
-function autoStyleTirage(){
-    if(cpt<25){
-        progressBarLength += 4;
-        resetBtnAchat();
-        document.querySelector("#normal").innerHTML = "Communes : 0%";
-        document.querySelector("#secret").innerHTML = "Secrètes : 0%";
-        document.querySelector("#superSecret").innerHTML = "Super Secrètes : 0%";
-    }else if(cpt >= 25 && cpt < 50){ // Tirage
-        if(cpt==25){resetProgressBar();}
-        document.querySelector("#normal").innerHTML = "Communes : 100%";
-        document.querySelector("#secret").innerHTML = "Secrètes : 0%";
-        document.querySelector("#superSecret").innerHTML = "Super Secrètes : 0%";
-        btnAchat.innerHTML = "&#62&#62&#62 Tirage &#60&#60&#60";
-        btnAchat.style.backgroundColor = "#B87333";
-        btnAchat.style.color = "#FFF";
-        btnAchat.style.border = "5px solid #fff";
-        progressBar.style.backgroundColor = "#C0C0C0";
-        progressBarLength += 4;
-        
-    } else if(cpt >= 50 && cpt < 100){ // Super Tirage
-        if(cpt==50){resetProgressBar();}
-        document.querySelector("#normal").innerHTML = "Communes : 95%";
-        document.querySelector("#secret").innerHTML = "Secrètes : 5%";
-        document.querySelector("#superSecret").innerHTML = "Super Secrètes : 0%";
-        btnAchat.style.backgroundColor = "#C0C0C0";
-        progressBar.style.backgroundColor = "#FFD700";
-        btnAchat.innerHTML = "&#62&#62&#62 Super Tirage &#60&#60&#60";
-        btnAchat.style.color = "#FFF";
-        btnAchat.style.border = "5px solid #fff";
-        progressBarLength += 100/(100-50);
-        
-    } else if(cpt >= 100 && cpt < 200){ // Méga Super Tirage
-        if(cpt==100){resetProgressBar();}
-        document.querySelector("#normal").innerHTML = "Communes : 88%";
-        document.querySelector("#secret").innerHTML = "Secrètes : 10%";
-        document.querySelector("#superSecret").innerHTML = "Super Secrètes : 2%";
-        btnAchat.style.backgroundColor = "#FFD700";
-        progressBar.style.backgroundColor = "#00b3ff";
-        btnAchat.innerHTML = "&#62&#62&#62 Méga Super Tirage &#60&#60&#60";
-        btnAchat.style.color = "#FFF";
-        btnAchat.style.border = "5px solid #fff";
-        progressBarLength += 100/(200-100);
-        
-    } else if(cpt >=200 && cpt < 400){ // Wati Tirage
-        if(cpt==200){resetProgressBar();}
-        document.querySelector("#normal").innerHTML = "Communes : 73%";
-        document.querySelector("#secret").innerHTML = "Secrètes : 20%";
-        document.querySelector("#superSecret").innerHTML = "Super Secrètes : 7%";
-        btnAchat.style.backgroundColor = "#00b3ff";
-        progressBar.style.backgroundColor = "#870b0b";
-        btnAchat.innerHTML = "&#62&#62&#62 Wati Tirage &#60&#60&#60";
-        btnAchat.style.color = "#FFF";
-        btnAchat.style.border = "5px solid #fff";
-        progressBarLength += 100/(400-200);
-        
-    } else if(cpt >= 400 && cpt < 800){ // Tirage de fou malade
-        if(cpt==400){resetProgressBar();}
-        document.querySelector("#normal").innerHTML = "Communes : 55%";
-        document.querySelector("#secret").innerHTML = "Secrètes : 35%";
-        document.querySelector("#superSecret").innerHTML = "Super Secrètes : 10%";
-        btnAchat.style.backgroundColor = "#870b0b";
-        progressBar.style.backgroundColor = "#fff";
-        btnAchat.innerHTML = "&#62&#62&#62 Tirage de fou malade &#60&#60&#60";
-        btnAchat.style.color = "#FFF";
-        btnAchat.style.border = "5px solid #fff";
-        progressBarLength += 100/(800-400);
-        
-    } else if(cpt >= 800){ // Tirage de la mort qui tue
-        document.querySelector("#normal").innerHTML = "Communes : 30%";
-        document.querySelector("#secret").innerHTML = "Secrètes : 50%";
-        document.querySelector("#superSecret").innerHTML = "Super Secrètes : 20%";
-        btnAchat.style.color = "#fff";
-        btnAchat.style.backgroundColor = autoHue();
-        progressBar.style.backgroundColor = autoHue();
-        btnAchat.style.color = "#FFF";
-        btnAchat.style.border = "5px solid #fff";
-        btnAchat.innerHTML = "&#62&#62&#62 Tirage de la mort qui tue &#60&#60&#60";        
-    }
-}
 
-// rénitialise les paramètres de la bare de progression
-function resetProgressBar(){
-    progressBar.style.width = "0%";
-    progressBarLength = 0;
-    btnAchat.style.color = "#fff";
-    btnAchat.style.border = "5px solid #fff";
-}
 
-function resizeProgressBar(){
-    if(cpt < 25){
-        progressBarLength = (cpt*4);
-        progressBar.style.width = progressBarLength+"%";
-        progressBarLength += 4;
-    } else if(cpt < 50){
-        cpt -=25;
-        progressBarLength = (cpt*4);
-        progressBar.style.width = progressBarLength+"%";
-        progressBarLength += 4;
-        cpt += 25;
-    } else if(cpt < 100){
-        progressBarLength = (cpt*8);
-        progressBar.style.width = progressBarLength+"%";
-        progressBarLength += 8;
-    } else if(cpt < 200){
-        progressBarLength = (cpt*16);
-        progressBar.style.width = progressBarLength+"%";
-        progressBarLength += 16;
-    } else if(cpt < 400){
-        progressBarLength = (cpt*32);
-        progressBar.style.width = progressBarLength+"%";
-        progressBarLength += 32;
-    }
-}
-
-// rénitialise les paramètres du bouton d'achat
-function resetBtnAchat(){
-    progressBar.style.backgroundColor = "#B87333";
-    btnAchat.style.backgroundColor = "#fff";
-    btnAchat.style.color = "#969696";
-    btnAchat.style.border = "5px solid #969696";
-    btnAchat.innerHTML = "Pas assez de Pouets";
-}
 
 // enlève des pouets et tire une carte
 function tirage(){
@@ -315,9 +208,11 @@ function tirage(){
 // appellée dans tirage() permet de lancer l'interface de piochage
 async function pickACard(param){
     if (param == 0){ // Un item est tiré puis affiché
-        var randomInt = getRandomIntInclusive(0, 25); // Total des collectables dans le 2eme paramètre de g
+        var randomInt = getRandomIntInclusive(0, 47); // Total des collectables dans le 2eme paramètre
         numberToCollectable(randomInt)
             .then(async collectable => {
+                ajoutCollection(randomInt);
+                saveCollection();
                 var img = "./images/collectables/"+collectable.img;
                 document.querySelector(".radial-light").style.visibility = "hidden";
                 document.querySelector(".picked-card").src = img;
@@ -358,7 +253,6 @@ async function pickACard(param){
         document.querySelector(".picked-card").style.top = "100%";
         reveal = false;
         if(nbTirages > 0){
-            console.log(nbTirages);
             await sleep(500);
             pickACard(0);
         }
@@ -398,47 +292,9 @@ function fermerCollection(){
     document.querySelector(".collection-frame").style.visibility = "hidden";
 }
 
-////// Partie sauvegarde
-document.querySelector("#sauvegarde").onclick = function () { 
-    save();
-    alert("Les données sont dans le presse papier");
-};
 
-function save(){
-    var data = "Pouets:"+cpt+";Cartes:"+cardsOwned+";Cartes_s:"+sCardsOwned+";Cartes_ss:"+ssCardsOwned+";";
-    data = btoa(data);
-    if(checkCookie("Cokie")){document.cookie ="Data ="+data+" ; expires=Thu, 01 jan 2030 12:12:12 UTC";}
-    navigator.clipboard.writeText(data);
-}
 
-// cette fonction permet de charger les données de sauvegardes depuis le cookie
-document.querySelector("#charger").onclick = function (){
-    load();
-}
-addEventListener("load",function(){load();});
-function load(){
-    if(document.querySelector("#load-input").value !== ""){
-        var data = atob(document.querySelector("#load-input").value).split(";");
-    } else {
-    var data = atob(getCookie("Data")).split(";");
-    }
-    console.log(data)
-    data.forEach(element => recup(element));
-    document.querySelector("#load-input").value = "";
-}
-document.querySelector("#load-input").addEventListener("keypress",function (event){
-    if (event.key==='Enter'){
-        event.preventDefault();
-        if(document.querySelector("#load-input").value !== ""){
-            var data = atob(document.querySelector("#load-input").value).split(";");
-        } else {
-        var data = atob(getCookie("Data")).split(";");
-        }
-        console.log(data)
-        data.forEach(element => recup(element));
-        document.querySelector("#load-input").value = "";
-    }
-});
+
 
 // Fonction permettant de recupérer la valeur d'un cookie
 function getCookie(cname) {
@@ -471,33 +327,6 @@ function checkCookie(cookieName) {
     return false;
   }
 
-//recupere les données de sauvegarde pour les mettre dans la partie
-function recup(element){
-    if(element.split(":")[0]=="Pouets"){
-        cpt = element.split(":")[1] -1;
-        cpt+=1;
-        document.querySelector(".pouet-display").innerHTML = cpt + " Pouets !";
-        resizeProgressBar();
-        resetBtnAchat();
-        autoStyleTirage();
-    } else if(element.split(":")[0]=="Cartes" &&  element.split(":")[1] !== ""){
-        cardsOwned = [];
-        element.split(":")[1].split(",").forEach((element) => {if(!cardsOwned.includes(element)){cardsOwned.push(element);}});
-    } else if(element.split(":")[0]=="Cartes_s" &&  element.split(":")[1] !== ""){
-        sCardsOwned = [];
-        element.split(":")[1].split(",").forEach((element) => {if(!sCardsOwned.includes(element)){sCardsOwned.push(element);}});
-    } else if(element.split(":")[0]=="Cartes_ss" &&  element.split(":")[1] !== ""){
-        ssCardsOwned = [];
-        element.split(":")[1].split(",").forEach((element) => {if(!ssCardsOwned.includes(element)){ssCardsOwned.push(element);}});
-    }
-}
-
-// affichage pop up cokie
-if(!checkCookie("Cokie")){
-    document.querySelector(".cookie-pop-up").style.visibility = "visible";
-}
-document.querySelector("#cokie-non").onclick = function(){document.querySelector(".cookie-pop-up").style.visibility = "hidden";};
-document.querySelector("#cokie-oui").onclick = function(){document.querySelector(".cookie-pop-up").style.visibility = "hidden"; document.cookie="Cokie = casse toi de la ! ; expires=Thu, 01 jan 2030 12:12:12 UTC"};
 
 
 
@@ -506,13 +335,13 @@ document.querySelector("#cokie-oui").onclick = function(){document.querySelector
 
 
 ////// Partie shop
-function afficherShop() {
+function afficherFenetre(param) {
     // window.alert("Le shop n'est pas encore disponible");
-    const shopElement = document.querySelector("#shop");
+    const shopElement = document.querySelector("#"+param);
     shopElement.style.visibility = "visible";
   
     // Appliquer un curseur en attente lorsque vous ne survolez pas #shop
-    const elements = document.querySelectorAll(":not(#shop)");
+    const elements = document.querySelectorAll(":not(#"+param+")");
     elements.forEach(function (element) {
       element.style.cursor = "wait";
     });
@@ -524,12 +353,11 @@ function afficherShop() {
         element.style.cursor = "default";
       });
     });
-    console.log("shop");
 }
 
 // Fermer le shop
-function fermerShop() {
-    const shopElement = document.querySelector("#shop");
+function fermerFenetre(param) {
+    const shopElement = document.querySelector("#"+param);
     shopElement.style.visibility = "hidden";
   
     // Réinitialiser le curseur
@@ -540,14 +368,18 @@ function fermerShop() {
 }
 
 // Onglets de la boutique
-function tabShop(tab) {
+function tabFenetre(tab, fenetre) {
     for(let i=0; i<3; i++){
         if (i!=tab){
-            document.querySelector("#tab-shop-"+i).ariaSelected = false;
-            document.querySelector("#content-shop-"+i).hidden = true;
+            document.querySelector("#tab-"+fenetre+"-"+i).ariaSelected = false;
+            if(fenetre == "shop"){
+                document.querySelector("#content-"+fenetre+"-"+i).hidden = true;
+            }
         } else {
-            document.querySelector("#tab-shop-"+i).ariaSelected = true;
-            document.querySelector("#content-shop-"+i).hidden = false;
+            document.querySelector("#tab-"+fenetre+"-"+i).ariaSelected = true;
+            if(fenetre == "shop"){
+                document.querySelector("#content-"+fenetre+"-"+i).hidden = false;
+            }
         }
     }
 }
@@ -568,6 +400,7 @@ radioButtons.forEach((radioButton) => {
             window.alert("Nombre de Pouets insuffisant");
         } else {
             cpt -= prix;
+            savePouet();
             document.querySelector(".btn-achat").innerHTML = cpt + " Pouets !";
             isPicked = 0;
             pickACard(0);
@@ -588,4 +421,69 @@ function numberToCollectable(number) {
       .then(data => {
         return data.collectables[number]; // Retourne l'objet collectable correspondant au nombre
       });
-  }
+}
+
+function afficherItems(page) {
+    for (var i = 0 + 24 * (page - 1); i < 24 * page; i++) {
+        (function (index) { // Utilisez une IIFE pour capturer la valeur actuelle de i
+            numberToCollectable(index)
+                .then(collectable => {
+                    document.querySelector("#item-" + index%24).innerHTML = "<div style=\"display: flex; justify-content: center; align-items: center; height: 95px;\"><img src=\"./images/collectables/" + collectable.img + "\" style=\"max-width: 95px; max-height: 95px; filter: brightness(0);\"></div>";
+                    if(itemsCollection.hasOwnProperty(index)){
+                        document.querySelector("#item-" + index%24).firstElementChild.firstElementChild.style.filter = "brightness(100%)";
+                    }
+                })
+                .catch(error => {
+                    console.error('Erreur lors de l appel à numberToCollectable :', error);
+                });
+        })(i); // Passez la valeur actuelle de i à l'IIFE
+    }
+}
+
+function ajoutCollection(item) {
+    if (!itemsCollection.hasOwnProperty(item)) {
+      itemsCollection[item] = 0;
+    }
+    itemsCollection[item] += 1;
+    console.log(itemsCollection);
+}
+
+function displayInfos(itemNumber){
+    if(itemsCollection.hasOwnProperty(itemNumber)){
+        numberToCollectable(itemNumber)
+        .then(collectable => {
+            document.querySelector("#inventaire-nom").innerHTML = "Nom : <strong>"+collectable.name+"</strong>";
+            document.querySelector("#inventaire-description").innerHTML = "Description : <strong>"+collectable.description+"</strong>";
+            document.querySelector("#inventaire-collection").innerHTML = "Dans la collection : <strong>"+itemsCollection[itemNumber]+"</strong>";
+        })
+        .catch(error => {
+            console.error('Erreur lors de l appel à numberToCollectable :', error);
+        });
+    } else {
+        document.querySelector("#inventaire-nom").innerHTML = "Nom : <strong>???</strong>";
+        document.querySelector("#inventaire-description").innerHTML = "Description : <strong>???</strong>";
+        document.querySelector("#inventaire-collection").innerHTML = "Dans la collection : <strong>0</strong>";
+    }
+}
+
+function savePouet(){
+    var data = btoa(cpt);
+    document.cookie = "Pouets ="+data+" ; expires=Thu, 01 jan 2030 12:12:12 UTC";
+}
+
+function saveCollection(){
+    const data = JSON.stringify(itemsCollection);
+    const dataB64 = btoa(data);
+    document.cookie = "Collection ="+encodeURIComponent(dataB64)+" ; expires=Thu, 01 jan 2030 12:12:12 UTC";
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    if(checkCookie("Pouets")){
+        cpt = parseInt(atob(getCookie("Pouets")));
+        document.querySelector(".btn-achat").innerHTML = cpt + " Pouets !";
+    }
+    if(checkCookie("Collection")){
+        const data = atob(decodeURIComponent(getCookie("Collection")));
+        itemsCollection = JSON.parse(data);
+    }
+});
